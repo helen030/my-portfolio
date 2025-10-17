4f9a8b628d3a5d32ad7fdd1cd998bbaa347acb5c
Helen Do <153411876+helen030@users.noreply.github.com>
2025-10-18 00:12:26 +1100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   site: 'https://helen030.github.io',
-  base: '/my-portfolio/',
+  base: '/my-portfolio/', // <-- must match repo name exactly
 });
