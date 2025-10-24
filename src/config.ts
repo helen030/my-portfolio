939560d2264be420c9517ba3c240231d09e876eb
Helen Do <153411876+helen030@users.noreply.github.com>
2025-10-24 23:51:39 +1100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const siteConfig = {
   social: {
     email: "thudo0311@gmail.com",
     linkedin: "https://www.linkedin.com/in/minh-thu-helen-do-502717232/",
-    github: "",
+    github: "https://github.com/helen030/DataProject",
   },
   aboutMe:
     "TBU",
@@ -15,37 +15,37 @@ export const siteConfig = {
     {
       name: "Gender-Based Violence Dashboard",
       description:
-        "Violence doesn’t strike at random—it follows patterns hidden in data. With Power BI, I visualized Kenya’s GBV data to uncover who’s most at risk, when danger peaks, and how we can use those insights to protect the vulnerable.",
-      link: "https://github.com/helen030/GBVProject/blob/3a463cd875be8ce0dcfc836788d5e63398d624d8/Kenya's%20GBV%20Dashboard.md",
+        "Violence doesn’t strike at random—it follows patterns hidden in data. With Power BI, I visualised Kenya’s GBV data to uncover who’s most at risk, when danger peaks, and how we can use those insights to protect the vulnerable.",
+      link: "https://github.com/helen030/DataProject/blob/d255bbb9c5330033db6cc5d8762c44cfdf40e2f9/Kenya's%20GBV%20Dashboard.md",
       skills: ["Power BI", "Data cleaning", "Data modelling", "Data visualisation"],
     },
     {
       name: "UNSW Going Green Dashboard",
       description:
         "How well does one of Australia's top universities perform in terms of sustainability? Using Power BI and ETL techniques, I transformed UNSW's unstructured energy data into a live dashboard that enables its staff to monitor progress and track UNSW's performance against that of other universities. ",
-      link: "TBU",
+      link: "https://github.com/helen030/DataProject/blob/d255bbb9c5330033db6cc5d8762c44cfdf40e2f9/Going%20Green%20Dashboard.md",
       skills: ["Power BI", "Excel", "Data cleaning", "Data modelling", "Data visualisation"],
     },
     {
       name: "Analysing Hong Kong Pain Relief Medication Market",
       description:
         "In a crowded pain relief market, strategy is everything. I analysed sales, pricing, and consumer data to uncover what truly drives success—and helped my client, Poon, position their Joint and Muscle Pain Gel for maximum impact.",
-      link: "https://github.com/helen030/GBVProject/blob/3a463cd875be8ce0dcfc836788d5e63398d624d8/Analysing%20Hong%20Kong%20Pain%20Relief%20Medication%20Market.ipynb",
+      link: "TBU",
       skills: ["Python"],
     },
     {
       name: "Customer Churn Rate Analysis and Prediction Model",
       description:
         "Why do customers leave, and can businesses see it coming? Using data from 30K+ telecom users, I built predictive models to uncover key churn drivers and help the company retain high-value customers before they walk away.",
-      link: "https://github.com/helen030/DataProject/blob/b0cab196cb61f1f512639deedddc2af17ebcbbd7/Customer%20Churn%20Rate%20Prediction%20Model.R",
+      link: "TBU",
       skills: ["R", "Data manipulation", "Statistical modelling"],
     },
   ],
   experience: [
     {
-      company: "TBU",
-      title: "Intern",
-      dateRange: "May 2025 – Present",
+      company: "TL Consulting",
+      title: "Operations Transformation Intern",
+      dateRange: "Jun 2025 – Present",
       bullets: [
         "Identified operational gaps and developed actionable initiatives for process improvement.",
         "Reduced client’s average food waste by ~17% and food costs by ~5% within 2 months.",
